c3cf3075144ade52153f30e54c17ab11e85f7f31
Henrique Areias <henriquesareias@hotmail.com>
2016-09-26 00:25:28 -0300

    first commit Erros corrigidos!

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -14,6 +14,15 @@ public class PapelMoeda {
         this.quantidade = quantidade;
     }
 
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+
     public int getValor() {
         return valor;
     }
