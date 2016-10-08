a37237b8726687134a0abb0a8b327d2350e5a290
Gabrielmanfredi <mrmastertutoriais@gmail.com>
2016-10-08 00:05:05 -0300

    Update TicketMachineTest.java

--- Source Code Inspection/test/Test/TicketMachineTest.java
+++ Source Code Inspection/test/Test/TicketMachineTest.java
@@ -3,8 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Test;
 
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,27 +18,49 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Henrique
+ * @author Gabriel
  */
 public class TicketMachineTest {
-    
+
     public TicketMachineTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
+    }
+
+    @Test
+    public void inserirTest() {
+        TicketMachine tm = new TicketMachine(10);
+        try {
+            tm.inserir(5);
+        } catch (PapelMoedaInvalidaException ex) {
+            Logger.getLogger(TicketMachineTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        assertEquals(tm.getSaldo(), 5);
+    }
+
+    @Test
+    public void imprimirTest() throws PapelMoedaInvalidaException, SaldoInsuficienteException {
+
+        TicketMachine tm2 = new TicketMachine(5);
+        
+        tm2.inserir(10);
+        
+        System.out.println(tm2.imprimir());
+        assertEquals("R$10", tm2.imprimir());
     }
 
     // TODO add test methods here.
